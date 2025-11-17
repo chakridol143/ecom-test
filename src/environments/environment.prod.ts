ef996416c75b58d97e6610b6bea59b06c2d4066c
Chakri Chikkaala <chakridol143@gmail.com>
2025-11-17 17:16:10 +0530

    env file changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ecom-backend-production-5341.up.railway.app/'
+  apiUrl: 'https://ecom-backend-production-5341.up.railway.app'
 };
